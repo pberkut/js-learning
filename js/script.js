8412a0cdb28464478f9036c020f3de43d7c04245
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-10-29 15:03:58 +0300

    update script

--- js/script.js
+++ js/script.js
@@ -355,3 +355,68 @@
 //   // Change code above this line
 //   return message;
 // }
+
+// // Task £ 28
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   switch (password) {
+//     case null:
+//       message = 'Canceled by user!';
+//       break;
+//     case 'jqueryismyjam':
+//       message = 'Welcome!';
+//       break;
+//     default:
+//       message = 'Access denied, wrong password!';
+//   }
+
+//   return message;
+// }
+
+// console.log(checkPassword('mangohackzor'));
+// console.log(checkPassword(null));
+// console.log(checkPassword('polyhax'));
+// console.log(checkPassword('jqueryismyjam'));
+
+// Task 29
+function getShippingCost(country) {
+  let message;
+  // Change code below this line
+  let price;
+  switch (country) {
+    case 'China':
+      price = 100;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Chile':
+      price = 250;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Australia':
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Jamaica':
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    default:
+      message = 'Sorry, there is no delivery to your country';
+  }
+  // Change code above this line
+  return message;
+}
+
+console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
+console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
+// End task 29
