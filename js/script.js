b2ac2f67d842cca20c5b3e541af96e8843087f38
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-02 12:34:35 +0200

    update

--- js/script.js
+++ js/script.js
@@ -860,16 +860,67 @@
 // console.log(checkAge(20));
 // console.log(checkAge(15));
 
-// Example
-function checkPassword(password) {
-  const ADMIN_PASSWORD = 'jqueryiamyjam';
+// // Example
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryiamyjam';
 
-  if (password === ADMIN_PASSWORD) {
-    return 'Welcome!';
-  }
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
 
-  return 'Acces denied, wrong password!';
-}
+//   return 'Acces denied, wrong password!';
+// }
 
-console.log(checkPassword('pass'));
-console.log(checkPassword('jqueryiamyjam'));
+// console.log(checkPassword('pass'));
+// console.log(checkPassword('jqueryiamyjam'));
+
+// Task 2 - 3
+// function checkStorage(available, ordered) {
+//   let message;
+
+//   if (ordered === 0) {
+//     message = 'Your order is empty!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, not enough goods in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+
+//   return message;
+// }
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+
+//   return 'The order is accepted, our manager will contact you';
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+// // Task 2-9
+// function getExtremeElements(array) {
+//   const firstElement = array[0];
+//   const lastArrayElement = array.length - 1;
+//   const lastElement = array[lastArrayElement];
+//   return `${firstElement}, ${lastElement}`;
+// }
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
+
+// function getExtremeElements(array) {
+//   const lastArrayElement = array.length - 1;
+//   const arrayExtreme = [array[0], array[lastArrayElement]];
+
+//   return arrayExtreme;
+// }
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
