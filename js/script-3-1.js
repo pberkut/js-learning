884a7201fa2039b0832a2bc96de201a5fafc6550
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-10 00:34:03 +0200

    update

--- js/script-3-1.js
+++ js/script-3-1.js
@@ -300,3 +300,45 @@
 
 // const averageRating = Number((totalRating / books.length).toFixed(1));
 // console.log(averageRating);
+
+//* Task 3 - 7
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+
+// console.table(apartment);
+
+//* Task 3 - 10
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
+//
