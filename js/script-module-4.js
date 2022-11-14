24e8bbbc210d799a40f4e0fce7a6c6d26682a894
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-14 15:59:11 +0200

    update

--- js/script-module-4.js
+++ js/script-module-4.js
@@ -72,32 +72,105 @@
 // processCall('Mango');
 
 // * Example refactoring function код выше
-function processCall(recipient, onAvailable, onNotAvailable) {
-  // Имитируем доступность абонента случайным числом
-  const isRecipientAvailable = Math.random() > 0.5;
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Имитируем доступность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
 
-  if (!isRecipientAvailable) {
-    onNotAvailable(recipient);
-    return;
-  }
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
 
-  onAvailable(recipient);
-}
+//   onAvailable(recipient);
+// }
 
-function takeCall(name) {
-  console.log(`Соединяем с ${name}, ожидайте...`);
-  // Логика принятия звонка
-}
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+//   // Логика принятия звонка
+// }
 
-function activateAnsweringMachine(name) {
-  console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
-  // Логика активации автоответчика
-}
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+//   // Логика активации автоответчика
+// }
 
-function leaveHoloMessage(name) {
-  console.log(`Абонент ${name} недоступен, записываем голограмму.`);
-  // Логика записи голограммы
-}
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+//   // Логика записи голограммы
+// }
 
-processCall('Манго', takeCall, activateAnsweringMachine);
-processCall('Поли', takeCall, leaveHoloMessage);
+// processCall('Манго', takeCall, activateAnsweringMachine);
+// processCall('Поли', takeCall, leaveHoloMessage);
+
+//* Example forEach
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Индекс ${i}, значение ${numbers[i]}`);
+// }
+
+// // Перебирающий forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+//* Стрелочные функции
+// function classicAdd(a, b, c) {
+//   return a + b + c;
+// }
+
+// const arrowAdd = (a, b, c) => {
+//   return a + b + c;
+// };
+
+// console.log(classicAdd(1, 2, 3));
+// console.log(arrowAdd(1, 2, 3));
+
+// const add = a => {
+//   return a + 5;
+// };
+
+// console.log(add(1));
+
+// const greet = () => {
+//   console.log('Hello!');
+// };
+
+// greet();
+
+// * Example
+// function classicAdd(a, b, c) {
+//   return a + b + c;
+// }
+
+// const add = (a, b, c) => a + b + c;
+
+// console.log(classicAdd(1, 2, 3));
+// console.log(add(1, 2, 3));
+
+// * Example
+// const add = (...args) => {
+//   console.log(args);
+// };
+
+// add(1, 2, 3, 4);
+
+// *Example
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Объявление функции
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс  ${index}, значение ${number}`);
+// });
+
+// // Анонимная стрелочная функция
+// numbers.forEach((number, index) => {
+//   console.log(`Индекс  ${index}, значение ${number}`);
+// });
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс  ${index}, значение ${number}`);
+// };
+
+// numbers.forEach(logMessage);
