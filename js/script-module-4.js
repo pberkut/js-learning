677c953a8bba0f42a226ce64f443b9f016f178c6
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-16 15:59:54 +0200

    update

--- js/script-module-4.js
+++ js/script-module-4.js
@@ -600,4 +600,54 @@ console.log(getUserFriends(users));
 // console.log(allGenres);
 // console.log(uniqueGenres);
 
-//  * Task 4 - 20
+// * Example
+// const LOW_SCORE = 50;
+// const HIGH_SCORE = 80;
+// const students = [
+//   { name: 'Mango', score: 83 },
+//   { name: 'Poly', score: 59 },
+//   { name: 'Ajax', score: 37 },
+//   { name: 'Kiwi', score: 94 },
+//   { name: 'Houston', score: 64 },
+// ];
+
+// const best = students.filter(student => student.score >= HIGH_SCORE);
+// console.log(best); // Массив объектов с именами Mango и Kiwi
+
+// const worst = students.filter(student => student.score < LOW_SCORE);
+// console.log(worst); // Массив с одним объектом Ajax
+
+// // В коллбек-функции удобно деструктуризировать свойства объекта
+// const average = students.filter(({ score }) => score >= LOW_SCORE && score < HIGH_SCORE);
+// console.log(average); // Массив объектов с именами Poly и Houston
+
+//  * Task 4 - 21
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = 'Bernard Cornwell';
+// Change code below this line
+
+const topRatedBooks = books.filter(({ rating }) => rating >= MIN_RATING);
+const booksByAuthor = books.filter(({ author }) => author === AUTHOR);
+
+console.log(topRatedBooks);
+console.log(booksByAuthor);
