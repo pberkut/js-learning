872dfd9f01b2a62368a6c4a3c4b1e67060183454
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-21 14:55:41 +0200

    update

--- js/script-module-4.js
+++ js/script-module-4.js
@@ -1621,3 +1621,74 @@ console.log(getUserFriends(users));
 
 // console.table(getTotalBalanceByGender(users, 'male'));
 // console.table(getTotalBalanceByGender(users, 'female'));
+
+// * Example
+// const tweets = [
+//   { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+//   { id: '001', likes: 2, tags: ['html', 'css'] },
+//   { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+//   { id: '003', likes: 8, tags: ['css', 'react'] },
+//   { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+// ];
+
+// // Пройдем по всем элементам коллекции и добавим значения свойства tags
+// // к аккумулятору, начальное значение которого укажем пустым массивом [].
+// // На каждой итерации пушим в аккумулятор все элементы tweet.tags и возвращаем его.
+// const tags = tweets.reduce((allTags, tweet) => {
+//   allTags.push(...tweet.tags);
+
+//   return allTags;
+// }, []);
+
+// console.log(tags);
+
+// // Наверное сбор тегов не одиночная операция, поэтому напишем функцию
+// // для сбора тегов из коллекции
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+
+//     return allTags;
+//   }, []);
+
+// console.log(getTags(tweets));
+
+// * Example
+// const tweets = [
+//   { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+//   { id: '001', likes: 2, tags: ['html', 'css'] },
+//   { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+//   { id: '003', likes: 8, tags: ['css', 'react'] },
+//   { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+// ];
+
+// const getTags = tweets =>
+//   tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+
+//     return allTags;
+//   }, []);
+
+// const tags = getTags(tweets);
+
+// // Вынесем callback-функцию отдельно, а в reducе передадим ссылку на нее.
+// // Это стандартная практика если callback-функция довольно большая.
+
+// // Если в объекте-аккумуляторе acc нету своего свойства с ключем tag,
+// // то создаем его и записывает ему значение 0.
+// // В противном случае увеличиваем значение на 1.
+// const getTagStats = (acc, tag) => {
+//   if (!acc.hasOwnProperty(tag)) {
+//     acc[tag] = 0;
+//   }
+
+//   acc[tag] += 1;
+
+//   return acc;
+// };
+
+// // Начальное значение аккумулятора это пустой объект {}
+// const countTags = tags => tags.reduce(getTagStats, {});
+
+// const tagCount = countTags(tags);
+// console.log(tagCount);
--- js/script-module-5.js
+++ js/script-module-5.js
@@ -1,1 +1,46 @@
+// * Example fn.call()
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
 
+// const mango = {
+//   username: 'Манго',
+// };
+// const poly = {
+//   username: 'Поли',
+// };
+
+// greetGuest.call(mango, 'Добро пожаловать'); // Добро пожаловать, Манго.
+// greetGuest.call(poly, 'С приездом'); // С приездом, Поли.
+
+// * Exapmle fn.apply()
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
+
+// const mango = {
+//   username: 'Манго',
+// };
+// const poly = {
+//   username: 'Поли',
+// };
+
+// greetGuest.apply(mango, ['Добро пожаловать']); // Добро пожаловать, Манго.
+// greetGuest.apply(poly, ['С приездом']); // С приездом, Поли.
+
+// * Example fn.bind()
+function greet(clientName) {
+  return `${clientName}, добро пожаловать в «${this.service}».`;
+}
+
+const steam = {
+  service: 'Steam',
+};
+const steamGreeter = greet.bind(steam);
+steamGreeter('Манго'); // "Манго, добро пожаловать в «Steam»."
+
+const gmail = {
+  service: 'Gmail',
+};
+const gmailGreeter = greet.bind(gmail);
+gmailGreeter('Поли'); // "Поли, добро пожаловать в «Gmail»."
