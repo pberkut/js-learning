bd080e2c72a339657218f0f1dbf566cae346921f
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-11-22 16:51:56 +0200

    update

--- js/script-module-5.js
+++ js/script-module-5.js
@@ -89,3 +89,63 @@
 // console.log(pizzaPalace.order('Rage'));
 
 // * Task 5 - 2
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// * Task 5 - 3
+// const historyService = {
+//   orders: [
+//     { email: 'jacob@hotmail.com', dish: 'Burrito' },
+//     { email: 'solomon@topmail.net', dish: 'Burger' },
+//     { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//     { email: 'solomon@topmail.net', dish: 'Apple pie' },
+//     { email: 'jacob@hotmail.com', dish: 'Taco' },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders.map(order => `email: ${order.email} dish: ${order.dish}`).join(' - ');
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
+
+// console.log(historyService.getOrdersLog());
+// console.log(historyService.getEmails());
+// console.log(historyService.getOrdersByEmail('jacob@hotmail.com'));
+
+// * Task 5 - 4
