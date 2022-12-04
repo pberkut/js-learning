ce8fa5dd1bce20d9ddc71d5b25d1367322edafdf
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-04 10:42:08 +0200

    update

--- js/script-module-5.js
+++ js/script-module-5.js
@@ -843,6 +843,32 @@
 // Example 3 - User
 // Напиши клас User який створює об'єкт із властивостями login та email. Оголоси приватні властивості #login та #email, доступ до яких зроби через гетер та сетер login та email.
 
+// class User {
+//   #login;
+//   #email;
+
+//   constructor({ login, email } = {}) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+
+//   get login() {
+//     return this.#login;
+//   }
+
+//   set login(newLogin) {
+//     this.#login = newLogin;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
 // const mango = new User({
 //   login: 'Mango',
 //   email: 'mango@dog.woof',
@@ -860,48 +886,84 @@
 // console.log(poly.login); // Poly
 // poly.login = 'Polycutie';
 // console.log(poly.login); // Polycutie
-// // Example 4 - Нотатки
-// // Напиши клас Notes який керує колекцією нотаток у властивості items. Замітка це об'єкт із властивостями text та priority. Додай класу статичну властивість Priority, у якому зберігатиметься об'єкт із пріоритетами.
 
-// // {
-// //   LOW: 'low',
-// //   NORMAL: 'normal',
-// //   HIGH: 'high'
-// // }
-// // Додай методи addNote(note), removeNote(text) та updatePriority(text, newPriority).
+// Example 4 - Нотатки
+// Напиши клас Notes який керує колекцією нотаток у властивості items. Замітка це об'єкт із властивостями text та priority. Додай класу статичну властивість Priority, у якому зберігатиметься об'єкт із пріоритетами.
+
+// {
+//   LOW: 'low',
+//   NORMAL: 'normal',
+//   HIGH: 'high'
+// }
+// Додай методи addNote(note), removeNote(text) та updatePriority(text, newPriority).
+
+// class Notes {
+//   static Priority = {
+//     LOW: 'low',
+//     NORMAL: 'normal',
+//     HIGH: 'high',
+//   };
+
+//   constructor(note) {
+//     this.items = note;
+//   }
+
+//   addNote(note) {
+//     this.items.push(note);
+//   }
+
+//   removeNote(text) {
+//     this.items.forEach(element => {
+//       if (element.text.includes(text)) {
+//         this.items.splice(element, 1);
+//       }
+//     });
+//   }
+
+//   updateNote(text, newPriority) {
+//     // this.items.find(item => item.text === text);
+//     this.items.forEach(item => {
+//       if (item.text.includes(text)) {
+//         item.priority = newPriority;
+//       }
+//     });
+//   }
+// }
 
 // const myNotes = new Notes([]);
 
+// // console.log(myNotes);
+
 // myNotes.addNote({ text: 'Моя перша замітка', priority: Notes.Priority.LOW });
-// console.log(myNotes.items);
+// console.log('addNote 1: ', myNotes.items);
 
 // myNotes.addNote({
 //   text: 'Моя друга замітка',
 //   priority: Notes.Priority.NORMAL,
 // });
-// console.log(myNotes.items);
+// console.log('addNote 2:', myNotes.items);
 
 // myNotes.removeNote('Моя перша замітка');
-// console.log(myNotes.items);
+// console.log('removeNote', myNotes.items);
 
 // myNotes.updateNote('Моя друга замітка', Notes.Priority.HIGH);
-// console.log(myNotes.items);
+// console.log('updateNote', myNotes.items);
 
-// // Example 5 - Toggle
-// // Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false). За замовчуванням значення властивості on повинно бути false.
+// Example 5 - Toggle
+// Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false). За замовчуванням значення властивості on повинно бути false.
 
-// const firstToggle = new Toggle({ isOpen: true });
-// console.group('firstToggle');
-// console.log(firstToggle.on);
-// firstToggle.toggle();
-// console.log(firstToggle.on);
-// console.groupEnd('firstToggle');
+const firstToggle = new Toggle({ isOpen: true });
+console.group('firstToggle');
+console.log(firstToggle.on);
+firstToggle.toggle();
+console.log(firstToggle.on);
+console.groupEnd('firstToggle');
 
-// const secondToggle = new Toggle();
-// console.group('secondToggle');
-// console.log(secondToggle.on);
-// secondToggle.toggle();
-// console.log(secondToggle.on);
-// console.groupEnd('secondToggle');
+const secondToggle = new Toggle();
+console.group('secondToggle');
+console.log(secondToggle.on);
+secondToggle.toggle();
+console.log(secondToggle.on);
+console.groupEnd('secondToggle');
 
-// console.log(window);
+console.log(window);
