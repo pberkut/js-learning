52563bb932bb30fd68f4780312710b4fa9c7e85c
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-04 23:28:16 +0200

    update

--- js/script-module-6.js
+++ js/script-module-6.js
@@ -123,3 +123,46 @@
 // cc('A');
 
 // console.log(cc());
+
+// * Example
+// function checkObj(obj, checkProp) {
+//   // Змініть код лише під цим рядком
+
+//   return obj.hasOwnProperty(checkProp) ? obj[checkProp] : 'Not Found';
+//   // Змініть код лише над цим рядком
+// }
+
+// console.log(checkObj({ gift: 'pony', pet: 'kitten', bed: 'sleigh' }, 'gift'));
+// console.log(checkObj({ gift: 'pony', pet: 'kitten', bed: 'sleigh' }, 'house'));
+
+// * Exapmle
+// Налаштування
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette'],
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: [],
+  },
+  5439: {
+    albumTitle: 'ABBA Gold',
+  },
+};
+
+// Змініть код лише під цим рядком
+function updateRecords(records, id, prop, value) {
+  if (prop === 'tracks') {
+  } else {
+  }
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
