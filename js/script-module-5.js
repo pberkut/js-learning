5299ef8e740a76d57f09ac4182129a78486a532c
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-04 10:59:02 +0200

    update

--- js/script-module-5.js
+++ js/script-module-5.js
@@ -952,18 +952,35 @@
 // Example 5 - Toggle
 // Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false). За замовчуванням значення властивості on повинно бути false.
 
-const firstToggle = new Toggle({ isOpen: true });
-console.group('firstToggle');
-console.log(firstToggle.on);
-firstToggle.toggle();
-console.log(firstToggle.on);
-console.groupEnd('firstToggle');
+// class Toggle {
+//   constructor({ isOpen = false } = {}) {
+//     this.on = isOpen;
+//   }
 
-const secondToggle = new Toggle();
-console.group('secondToggle');
-console.log(secondToggle.on);
-secondToggle.toggle();
-console.log(secondToggle.on);
-console.groupEnd('secondToggle');
+//   toggle() {
+//     if (this.on === true) {
+//       this.on = false;
+//     } else {
+//       this.on = true;
+//     }
+//   }
+// }
 
-console.log(window);
+// const firstToggle = new Toggle({ isOpen: true });
+
+// // console.log(firstToggle);
+
+// console.group('firstToggle');
+// console.log(firstToggle.on);
+// firstToggle.toggle();
+// console.log(firstToggle.on);
+// console.groupEnd('firstToggle');
+
+// const secondToggle = new Toggle();
+// console.group('secondToggle');
+// console.log('Before toggle: ', secondToggle.on);
+// secondToggle.toggle();
+// console.log('After toggle:', secondToggle.on);
+// console.groupEnd('secondToggle');
+
+// console.log(window);
