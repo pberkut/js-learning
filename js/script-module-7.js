16c59c0ac82812e666f45b943335c2d9c9b22403
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-15 12:34:32 +0200

    update

--- js/script-module-7.js
+++ js/script-module-7.js
@@ -2,33 +2,44 @@
 // Общий слушатель\
 // фильтр цепи клика
 
-const container = document.querySelector('.js-container');
+// const container = document.querySelector('.js-container');
 
-container.addEventListener('click', onClick);
+// container.addEventListener('click', onClick);
 
-function onClick(evt) {
-  // pattern guard clauses
-  if (evt.target.nodeName !== 'BUTTON') {
-    return;
-  }
-  //   console.log('evt: ', evt);
-  console.log('evt.target: ', evt.target);
-  console.log('evt.currentTarget: ', evt.currentTarget);
-  console.log(evt.target.nodeName);
+// function onClick(evt) {
+//   // pattern guard clauses
+//   if (evt.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+//   //   console.log('evt: ', evt);
+//   console.log('evt.target: ', evt.target);
+//   console.log('evt.currentTarget: ', evt.currentTarget);
+//   console.log(evt.target.nodeName);
+// }
+
+// // Код добавления кнопок
+
+// const addBtn = document.querySelector('.js-add-btn');
+// let labelCounter = 6;
+
+// addBtn.addEventListener('click', onAddBtnClick);
+
+// function onAddBtnClick() {
+//   const btn = document.createElement('button');
+//   btn.textContent = `Кнопка ${labelCounter}`;
+//   btn.type = 'button';
+
+//   container.appendChild(btn);
+//   labelCounter += 1;
+// }
+
+// * Example Game
+const content = document.querySelector('.content');
+
+let markup = '';
+
+for (let i = 1; i <= 9; i += 1) {
+  markup += '<div class="item"></div>';
 }
 
-// Код добавления кнопок
-
-const addBtn = document.querySelector('.js-add-btn');
-let labelCounter = 6;
-
-addBtn.addEventListener('click', onAddBtnClick);
-
-function onAddBtnClick() {
-  const btn = document.createElement('button');
-  btn.textContent = `Кнопка ${labelCounter}`;
-  btn.type = 'button';
-
-  container.appendChild(btn);
-  labelCounter += 1;
-}
+content.insertAdjacentHTML('beforeend', markup);
