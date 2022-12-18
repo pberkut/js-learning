4b244c1fc88159885f9f154c1be541c22964ea77
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-18 13:14:34 +0200

    update

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,2 +1,12 @@
 const message = 'NodeJS in amazing!\n';
 console.log(message);
+
+const validator = require('validator');
+
+const validateEmail = email => {
+  return validator.isEmail(email);
+};
+
+console.log('Is mango@mail.com a valid email?: ', validateEmail('mango@mail.com'));
+
+console.log('Is Mangozedog.com a valid email?: ', validateEmail('Mangozedog.com'));
