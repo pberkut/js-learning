8c90795405631d34f7148150e8aabc2dcf611b40
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-01 13:21:00 +0200

    Create game Casino Slots

---
+++ src/js/module-9/07-games-slots.js
@@ -0,0 +1,10 @@
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//   .then(r => r.json())
+//   .then(pokemon => {
+//     console.log(pokemon);
+//   });
+
+// * Example
+// console.time('start');
+// console.log('%c Привет', 'color: green; font-size: 32px');
+// console.timeEnd('start');
