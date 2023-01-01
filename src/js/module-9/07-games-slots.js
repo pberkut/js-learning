044c69597b1a8222757ac69e5a3fa6cde8e0ffd4
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-01 15:09:26 +0200

    Finish game slot

--- src/js/module-9/07-games-slots.js
+++ src/js/module-9/07-games-slots.js
@@ -8,3 +8,83 @@
 // console.time('start');
 // console.log('%c Привет', 'color: green; font-size: 32px');
 // console.timeEnd('start');
+
+// *
+// Мікро процеси мають БІЛЬШИЙ пріорітет
+// 1. Promise
+// 2. Observer
+
+// Макро процеси мають МЕНЬШИЙ пріорітет
+// setTimeout
+// setInterval
+// setImmediate
+// requestAnimationFrame
+
+// ! Start code Game Slots
+import Notiflix from 'notiflix';
+
+const start = document.querySelector('.js-form');
+const container = document.querySelector('.js-container');
+const delay = 500;
+
+start.addEventListener('submit', startGame);
+
+function startGame(evt) {
+  evt.preventDefault();
+
+  const arrWin = [];
+  const arrLose = [];
+
+  const childrenLength = container.children.length;
+
+  const {
+    level: { value: level },
+    start,
+  } = evt.currentTarget.elements;
+
+  start.disabled = true;
+
+  for (let i = 0; i < childrenLength; i += 1) {
+    const item = container.children[i];
+
+    item.textContent = '';
+
+    createPromise(Number(level), '🔔', '💰', delay * i)
+      .then(resp => {
+        markField(item, resp);
+        arrWin.push(resp);
+      })
+      .catch(resp => {
+        markField(item, resp);
+        arrLose.push(resp);
+      })
+      .finally(() => {
+        if (i === childrenLength - 1) {
+          start.disabled = false;
+        }
+        const result = arrWin.length === childrenLength || arrLose.length === childrenLength;
+
+        if (result) {
+          Notiflix.Notify.success('You are WINNER! ');
+        }
+      });
+  }
+}
+
+function markField(item, smile) {
+  item.textContent = smile;
+}
+
+function createPromise(level, win, lose, delay) {
+  const random = Math.random();
+
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (random > level) {
+        res(win);
+      } else {
+        rej(lose);
+      }
+    }, delay);
+  });
+}
