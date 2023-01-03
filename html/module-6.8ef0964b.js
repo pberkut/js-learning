5dab77b63eb56536889045d958633991522ff4ea
pberkut <109954038+pberkut@users.noreply.github.com>
2023-01-03 08:45:43 +0000

    Deploying to gh-pages from @ pberkut/js-learning@6e3fc15cfd17d8ea93a21f9654c2f2b1b86ed07e 🚀

---
+++ html/module-6.8ef0964b.js
@@ -0,0 +1,2 @@
+const o=[2,3,4,5,6];let l=0;for(let e=0;e<o.length;e++)l+=o[e],console.log(l);console.log(l);
+//# sourceMappingURL=module-6.8ef0964b.js.map
--- html/module-6.e5a6df34.js
+++
@@ -1,2 +0,0 @@
-console.log("start");
-//# sourceMappingURL=module-6.e5a6df34.js.map
