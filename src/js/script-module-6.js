fa210b3670e247083a6e69ab9788ffef50f36980
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-03 14:37:24 +0200

    update

--- src/js/script-module-6.js
+++ src/js/script-module-6.js
@@ -449,3 +449,64 @@
 // }
 
 // console.log(sum([1, 2, 3], 3));
+
+// * Example
+// Setup
+// const contacts = [
+//   {
+//     firstName: 'Akira',
+//     lastName: 'Laine',
+//     number: '0543236543',
+//     likes: ['Pizza', 'Coding', 'Brownie Points'],
+//   },
+//   {
+//     firstName: 'Harry',
+//     lastName: 'Potter',
+//     number: '0994372684',
+//     likes: ['Hogwarts', 'Magic', 'Hagrid'],
+//   },
+//   {
+//     firstName: 'Sherlock',
+//     lastName: 'Holmes',
+//     number: '0487345643',
+//     likes: ['Intriguing Cases', 'Violin'],
+//   },
+//   {
+//     firstName: 'Kristian',
+//     lastName: 'Vos',
+//     number: 'unknown',
+//     likes: ['JavaScript', 'Gaming', 'Foxes'],
+//   },
+// ];
+
+// function lookUpProfile(name, prop) {
+//   // Only change code below this line
+//   for (let i = 0; i < contacts.length; i += 1) {
+//     const isHasProp = contacts[i][prop];
+
+//     if (contacts[i].firstName === name) {
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         return contacts[i][prop];
+//       }
+//       return 'No such property';
+//     }
+//   }
+//   return 'No such contact';
+//   // Only change code above this line
+// }
+
+// lookUpProfile('Akira', 'likes');
+// lookUpProfile('Kristian', 'lastName');
+// lookUpProfile('Sherlock', 'likes');
+// lookUpProfile('Harry', 'likes');
+// lookUpProfile('Bob', 'number');
+// lookUpProfile('Akira', 'address');
+
+// //
+// console.log(lookUpProfile('Akira', 'address'));
+// console.log(lookUpProfile('Kristian', 'lastName'));
+// console.log(lookUpProfile('Sherlock', 'likes'));
+// console.log(lookUpProfile('Harry', 'likes'));
+// console.log(lookUpProfile('Bob', 'number'));
+
+// * Example
