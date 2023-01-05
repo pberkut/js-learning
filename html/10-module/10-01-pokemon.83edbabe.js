34a71204c488362b32855d50ae4d6ae346b55c8a
pberkut <109954038+pberkut@users.noreply.github.com>
2023-01-05 00:56:58 +0000

    Deploying to gh-pages from @ pberkut/js-learning@ddc67b6e7eb944552ecd1f043473ac4c28ca47f9 🚀

--- html/10-module/10-01-pokemon.83edbabe.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-container");fetch("https://pokeapi.co/api/v2/pokemon/2").then((e=>e.json())).then((n=>{const o=markupPokemonTpl(n);e.innerHTML=o}));
-//# sourceMappingURL=10-01-pokemon.83edbabe.js.map
