0901f2aa46090f9bb4159680d0f07aa01831540f
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-05 18:03:05 +0200

    update

--- src/js/10-module/10-01-pokemon.js
+++ src/js/10-module/10-01-pokemon.js
@@ -1,10 +1,12 @@
 import throttle from 'lodash.throttle';
-
+import Notiflix from 'notiflix';
 import markupPokemonTpl from './templates/pokemon-card.hbs';
+import API from './api-service-pokemon';
 
 const container = document.querySelector('.js-container');
 const searchForm = document.querySelector('.form');
 const searchInput = document.querySelector('[name="query"]');
+window.addEventListener('keydown', onPresskeyEscape);
 
 searchInput.addEventListener('input', throttle(onSearch, 300));
 
@@ -12,13 +14,22 @@ function onSearch(e) {
   e.preventDefault();
   const input = e.target;
   const searchQuery = input.value;
-  console.dir(searchQuery);
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`)
-    .then(resp => resp.json())
-    .then(pokemon => {
-      const markup = markupPokemonTpl(pokemon);
-      container.innerHTML = markup;
-    })
-    .catch(err => console.error(err));
+  API.fetchPokemon(searchQuery).then(renderPokemonCard).catch(onFetchError);
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = markupPokemonTpl(pokemon);
+  container.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  Notiflix.Notify.failure(`Failed Id. Error: ${error}`);
+}
+
+function onPresskeyEscape(e) {
+  if (e.code === 'Escape') {
+    // console.log(e.code);
+    searchForm.reset();
+  }
 }
