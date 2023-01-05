1f952a479e5c91bfa02975be0562cd66c9dc739c
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-05 03:35:00 +0200

    udapte

--- src/js/10-module/10-01-pokemon.js
+++ src/js/10-module/10-01-pokemon.js
@@ -1,10 +1,24 @@
-import markupPokemonTpl from './templates/pokemon.hbs';
+import throttle from 'lodash.throttle';
+
+import markupPokemonTpl from './templates/pokemon-card.hbs';
 
 const container = document.querySelector('.js-container');
+const searchForm = document.querySelector('.form');
+const searchInput = document.querySelector('[name="query"]');
 
-fetch('https://pokeapi.co/api/v2/pokemon/5')
-  .then(resp => resp.json())
-  .then(pokemon => {
-    const markup = markupPokemonTpl(pokemon);
-    container.innerHTML = markup;
-  });
+searchInput.addEventListener('input', throttle(onSearch, 300));
+
+function onSearch(e) {
+  e.preventDefault();
+  const input = e.target;
+  const searchQuery = input.value;
+  console.dir(searchQuery);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`)
+    .then(resp => resp.json())
+    .then(pokemon => {
+      const markup = markupPokemonTpl(pokemon);
+      container.innerHTML = markup;
+    })
+    .catch(err => console.error(err));
+}
