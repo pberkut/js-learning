d516450261cbc2956bb68822862032c0dedb6c5d
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-13 04:24:53 +0200

    Completed 10-04 Task. Infinity Scrol Pokemon RESTful API

--- src/js/10-module/10-04/10-04-main-script.js
+++ src/js/10-module/10-04/10-04-main-script.js
@@ -1,25 +1,60 @@
+import Notiflix from 'notiflix';
 import markupPokemonCardTmp from './templates/pokemon-card.hbs';
 import PokemonApiService from './pokemon-api-service';
 
 const containerRef = document.querySelector('.js-container');
+const guard = document.querySelector('.js-guard');
+const options = {
+  root: null,
+  rootMargin: '400px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoad, options);
 
 const pokemonApiService = new PokemonApiService();
 
-pokemonApiService.fetchPokemons().then(getPokemons);
+start();
+
+function start() {
+  pokemonApiService
+    .fetchPokemons()
+    .then(getPokemons)
+    .catch(err => {
+      Notiflix.Notify.failure(err);
+    });
+}
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log('Observer');
+      pokemonApiService.offsetNum += 5;
+      //   pokemonApiService.increment(20);
+      start();
+    }
+  });
+}
 
 function getPokemons(pokemons) {
-  //   console.log(pokemons);
-
   for (const pokemon of pokemons.results) {
     fetch(pokemon.url)
-      .then(resp => resp.json())
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error(resp.statusText);
+        }
+        return resp.json();
+      })
       .then(pokemon => {
-        console.log(pokemon);
         appendPokemonMarkup(pokemon);
+      })
+      .catch(err => {
+        Notiflix.Notify.failure(err);
       });
   }
 }
 
 function appendPokemonMarkup(pokemon) {
   containerRef.insertAdjacentHTML('beforeend', markupPokemonCardTmp(pokemon));
+  observer.observe(guard);
 }
--- src/js/10-module/10-04/pokemon-api-service.js
+++ src/js/10-module/10-04/pokemon-api-service.js
@@ -1,29 +1,37 @@
-import Notiflix from 'notiflix';
-
 const BASE_URL = 'https://pokeapi.co/api/v2';
 const options = {
   method: 'GET',
 };
 
 export default class PokemonApiService {
-  constructor() {}
+  constructor() {
+    this.offset = 5;
+  }
 
   fetchPokemons() {
     const endPoint = 'pokemon';
     const id = '1';
-    const pagination = 20;
+    // const offset = 20;
     const url = `${BASE_URL}/${endPoint}`;
 
-    return fetch(`${url}?limit=${pagination}`, options)
-      .then(resp => {
-        if (!resp.ok) {
-          throw new Error(resp.statusText);
-        }
+    return fetch(`${url}?limit=10&offset=${this.offset}`, options).then(resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
 
-        return resp.json();
-      })
-      .catch(err => {
-        Notiflix.Notify.failure(err);
-      });
+      return resp.json();
+    });
+  }
+
+  increment(amount) {
+    this.offset += amount;
+  }
+
+  get offsetNum() {
+    return this.offset;
+  }
+
+  set offsetNum(newAmount) {
+    this.offset = newAmount;
   }
 }
