43f212774b84b78037b0d3d25b1f5522642e4693
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-19 15:55:06 +0200

    JSON-server use FETCH

---
+++ src/js/11-module/11-02/11-02-main-script.js
@@ -0,0 +1,73 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+// getPost(1).then(console.log).catch(console.log);
+// getAllPost().then(console.log).catch(console.log);
+// createPost().then(console.log).catch(console.log);
+// updatePost(2).then(console.log).catch(console.log);
+// changePost(3).then(console.log).catch(console.log);
+
+function getAllPost() {
+  return fetch(`${BASE_URL}`).then(r => r.json());
+}
+
+function getPost(postId) {
+  return fetch(`${BASE_URL}/${postId}`).then(r => r.json());
+}
+
+function createPost() {
+  const myPost = {
+    title: 'First title',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim perferendis earum, magni nesciunt quod unde eligendi repellendus, maiores cumque cupiditate praesentium, harum veritatis aliquam delectus hic quibusdam! Quo, facere!',
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(myPost),
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+    },
+  };
+
+  return fetch(BASE_URL, options).then(r => r.json());
+}
+
+function updatePost(postId) {
+  const myPost = {
+    title: 'I update this title',
+    body: 'I update this text in body',
+  };
+
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(myPost),
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+    },
+  };
+
+  return fetch(`${BASE_URL}/${postId}`, options).then(r => r.json());
+}
+
+function changePost(postId) {
+  const myPost = {
+    title: 'This title change from VSCode',
+  };
+
+  const options = {
+    method: 'PATCH',
+    body: JSON.stringify(myPost),
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+    },
+  };
+
+  return fetch(`${BASE_URL}/${postId}`, options).then(r => r.json());
+}
+
+function removePost(postId) {
+  options = {
+    method: 'DELETE',
+  };
+
+  return fetch(`${BASE_URL}/${postId}`, options).then(r => r.json());
+}
