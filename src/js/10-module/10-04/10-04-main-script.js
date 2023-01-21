69bb552c758bae14b9f58a70912ba118c5007489
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-21 09:58:34 +0200

    update 10-04

--- src/js/10-module/10-04/10-04-main-script.js
+++ src/js/10-module/10-04/10-04-main-script.js
@@ -1,9 +1,11 @@
 import Notiflix from 'notiflix';
 import markupPokemonCardTmp from './templates/pokemon-card.hbs';
-import PokemonApiService from './pokemon-api-service';
+import { PokeAPI } from './pokemon-api-service';
 
 const containerRef = document.querySelector('.js-container');
 const guard = document.querySelector('.js-guard');
+const pokeAPI = new PokeAPI();
+
 const options = {
   root: null,
   rootMargin: '500px',
@@ -12,54 +14,33 @@ const options = {
 
 let observer = new IntersectionObserver(onLoad, options);
 
-const pokemonApiService = new PokemonApiService();
-
 start();
 
-function start() {
-  pokemonApiService
-    .fetchPokemons()
-    .then(getPokemons)
-    .catch(err => {
-      Notiflix.Notify.failure(err);
-    });
+async function start() {
+  await getPokemons();
+  // debugger;
 }
 
 function onLoad(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      console.log('Observer');
-      // pokemonApiService.offsetNum += 5;
-      pokemonApiService.increment(5);
-      pokemonApiService
-        .fetchPokemons()
-        .then(getPokemons)
-        .catch(err => {
-          Notiflix.Notify.failure(err);
-        });
+      pokeAPI.nextPage();
+      getPokemons();
     }
   });
 }
 
-function getPokemons(pokemons) {
-  for (const pokemon of pokemons.results) {
-    fetch(pokemon.url)
-      .then(resp => {
-        if (!resp.ok) {
-          throw new Error(resp.statusText);
-        }
-        return resp.json();
-      })
-      .then(pokemon => {
-        appendPokemonMarkup(pokemon);
-      })
-      .catch(err => {
-        Notiflix.Notify.failure(err);
-      });
+async function getPokemons() {
+  try {
+    const pokemons = await pokeAPI.getPokemons();
+
+    appendPokemonMarkup(pokemons);
+  } catch (err) {
+    Notiflix.Notify.failure(err);
   }
 }
 
-function appendPokemonMarkup(pokemon) {
-  containerRef.insertAdjacentHTML('beforeend', markupPokemonCardTmp(pokemon));
+function appendPokemonMarkup(pokemons) {
+  containerRef.insertAdjacentHTML('beforeend', markupPokemonCardTmp({ pokemons: pokemons }));
   observer.observe(guard);
 }
--- src/js/10-module/10-04/pokemon-api-service.js
+++ src/js/10-module/10-04/pokemon-api-service.js
@@ -1,37 +1,67 @@
-const BASE_URL = 'https://pokeapi.co/api/v2';
-const options = {
-  method: 'GET',
-};
+// const BASE_URL = 'https://pokeapi.co/api/v2';
 
-export default class PokemonApiService {
-  constructor() {
-    this.offset = 0;
-  }
+// const options = {
+//   method: 'GET',
+// };
 
-  fetchPokemons() {
-    const endPoint = 'pokemon';
-    const id = '1';
-    // const offset = 20;
-    const url = `${BASE_URL}/${endPoint}`;
+// export default class PokemonApiService {
+//   constructor() {
+//     this.offset = 0;
+//   }
 
-    return fetch(`${url}?limit=20&offset=${this.offset}`, options).then(resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
+//   fetchPokemons() {
+//     const endPoint = 'pokemon';
+//     const id = '1';
+//     // const offset = 20;
+//     const url = `${BASE_URL}/${endPoint}`;
 
-      return resp.json();
+//     return fetch(`${url}?limit=20&offset=${this.offset}`, options).then(resp => {
+//       if (!resp.ok) {
+//         throw new Error(resp.statusText);
+//       }
+
+//       return resp.json();
+//     });
+//   }
+
+//   increment(amount) {
+//     this.offset += amount;
+//   }
+
+//   get offsetNum() {
+//     return this.offset;
+//   }
+
+//   set offsetNum(newAmount) {
+//     this.offset = newAmount;
+//   }
+// }
+
+import axios from 'axios';
+
+class PokeAPI {
+  #BASE_URL = 'https://pokeapi.co/api/v2';
+  #offset = 0;
+
+  async getPokemons() {
+    const response = await axios.get(`${this.#BASE_URL}/pokemon?limit=20&offset=${this.#offset}`);
+    const pokemons = response.data.results;
+
+    const responses = pokemons.map(async pokemon => {
+      const pokem = await axios.get(pokemon.url);
+      return pokem.data;
     });
+
+    const data = await Promise.allSettled(responses);
+
+    return data.filter(response => response.status === 'fulfilled').map(response => response.value);
   }
 
-  increment(amount) {
-    this.offset += amount;
+  nextPage() {
+    this.#offset += 20;
   }
 
-  get offsetNum() {
-    return this.offset;
-  }
-
-  set offsetNum(newAmount) {
-    this.offset = newAmount;
-  }
+  hasMorePokemons() {}
 }
+
+export { PokeAPI };
