dbd078dcd6e85e1c7f2e75f1c282b848c9caebb9
pberkut <109954038+pberkut@users.noreply.github.com>
2023-01-25 17:51:06 +0000

    Deploying to gh-pages from @ pberkut/js-learning@54401b3004404cdc589380dc65b3938c2c0cd7f3 🚀

---
+++ html/6-module.761b0aad.js
@@ -0,0 +1,2 @@
+var e="Rocky";document.getElementById("geeks").innerHTML="string- My pet name is "+e,document.getElementById("Geeks").innerHTML="string- My pet name is "+e;
+//# sourceMappingURL=6-module.761b0aad.js.map
--- html/6-module.89dc5290.js
+++
@@ -1,2 +0,0 @@
-var r=new ArrayBuffer(8);new Int32Array(r);let e="5asdf";console.log(e);
-//# sourceMappingURL=6-module.89dc5290.js.map
