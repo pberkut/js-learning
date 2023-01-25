d408a9e02514860ccf964e298177875759a40106
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-01-25 08:48:15 +0200

    update

--- src/js/script-module-6.js
+++ src/js/script-module-6.js
@@ -706,59 +706,75 @@
 
 // program to implement queue data structure
 
-class Queue {
-  constructor() {
-    this.items = [];
-  }
+// class Queue {
+//   constructor() {
+//     this.items = [];
+//   }
 
-  // add element to the queue
-  enqueue(element) {
-    return this.items.push(element);
-  }
+//   // add element to the queue
+//   enqueue(element) {
+//     return this.items.push(element);
+//   }
 
-  // remove element from the queue
-  dequeue() {
-    if (this.items.length > 0) {
-      return this.items.shift();
-    }
-  }
+//   // remove element from the queue
+//   dequeue() {
+//     if (this.items.length > 0) {
+//       return this.items.shift();
+//     }
+//   }
 
-  // view the last element
-  peek() {
-    return this.items[this.items.length - 1];
-  }
+//   // view the last element
+//   peek() {
+//     return this.items[this.items.length - 1];
+//   }
 
-  // check if the queue is empty
-  isEmpty() {
-    return this.items.length == 0;
-  }
+//   // check if the queue is empty
+//   isEmpty() {
+//     return this.items.length == 0;
+//   }
 
-  // the size of the queue
-  size() {
-    return this.items.length;
-  }
+//   // the size of the queue
+//   size() {
+//     return this.items.length;
+//   }
 
-  // empty the queue
-  clear() {
-    this.items = [];
-  }
+//   // empty the queue
+//   clear() {
+//     this.items = [];
+//   }
+// }
+
+// let queue = new Queue();
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(4);
+// queue.enqueue(8);
+// console.log(queue.items);
+
+// queue.dequeue();
+// console.log(queue.items);
+
+// console.log(queue.peek());
+
+// console.log(queue.isEmpty());
+
+// console.log(queue.size());
+
+// queue.clear();
+// console.log(queue.items);
+
+// * Example
+var buffer = new ArrayBuffer(8);
+var view = new Int32Array(buffer);
+
+let a = 5;
+let b = 'asdf';
+let c = a + b;
+
+// if you want to see the value of c
+console.log(c);
+
+// then do other operations
+if (c) {
+  // do something
 }
-
-let queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(4);
-queue.enqueue(8);
-console.log(queue.items);
-
-queue.dequeue();
-console.log(queue.items);
-
-console.log(queue.peek());
-
-console.log(queue.isEmpty());
-
-console.log(queue.size());
-
-queue.clear();
-console.log(queue.items);
