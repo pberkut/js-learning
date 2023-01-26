13ecdc31427af25e510507657c5f754220da1a6a
pberkut <109954038+pberkut@users.noreply.github.com>
2023-01-26 19:21:21 +0000

    Deploying to gh-pages from @ pberkut/js-learning@1ef4cb0523df5f20c7212a07151d79fec8d95303 🚀

--- html/6-module.761b0aad.js
+++
@@ -1,2 +0,0 @@
-var e="Rocky";document.getElementById("geeks").innerHTML="string- My pet name is "+e,document.getElementById("Geeks").innerHTML="string- My pet name is "+e;
-//# sourceMappingURL=6-module.761b0aad.js.map
