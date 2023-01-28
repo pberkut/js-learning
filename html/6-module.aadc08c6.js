20d85559a9ddb05e24f811ece0d112b432374a19
pberkut <109954038+pberkut@users.noreply.github.com>
2023-01-28 20:50:46 +0000

    Deploying to gh-pages from @ pberkut/js-learning@fa4fcc32f113aac35d48ad17cdb9566e4ccdc332 🚀

---
+++ html/6-module.aadc08c6.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=6-module.aadc08c6.js.map
--- html/6-module.aeec1c91.js
+++
@@ -1,2 +0,0 @@
-const t=prompt("Enter operator to perform the calculation ( either +, -, * or / ): "),e=parseFloat(prompt("Enter the first number: ")),r=parseFloat(prompt("Enter the second number: "));let o;o="+"==t?e+r:"-"==t?e-r:"*"==t?e*r:e/r,window.alert(" Result is"+o);
-//# sourceMappingURL=6-module.aeec1c91.js.map
