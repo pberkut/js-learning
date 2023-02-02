6b488630991310cdab0284640b40622102b5c2d7
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-02 09:37:50 +0200

    update

--- src/js/script-module-6.js
+++ src/js/script-module-6.js
@@ -1019,3 +1019,30 @@ document.write(a); */
 //         console.log('Unknown topic', topic);
 //     }
 // });
+
+// * Example 
+// let SSL_KEY = __dirname + '/certs/key.pem';
+// let SSL_CERT = __dirname + '/certs/certificate.pem';
+// let MONGOURL = 'mongodb://admin:admin123@ds125481.mlab.com:25481/iotjs';
+
+// module.exports = {
+//     id: 'broker',
+//     stats: false,
+//     port: 8443,
+//     logger: {
+//         name: 'iotjs',
+//         level: 'debug'
+//     },
+//     secure: {
+//         keyPath: SSL_KEY,
+//         certPath: SSL_CERT,
+//     },
+//     backend: {
+//         type: 'mongodb',
+//         url: MONGOURL
+//     },
+//     persistence: {
+//         factory: 'mongo',
+//         url: MONGOURL
+//     }
+// };
