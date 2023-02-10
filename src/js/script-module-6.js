a9eada6459816972758acf536b3c725a90a65374
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-10 12:47:40 +0200

    update

--- src/js/script-module-6.js
+++ src/js/script-module-6.js
@@ -1136,3 +1136,26 @@ document.write(a); */
 //   }
 
 //   addDarkClassToHtml();
+
+// * Example
+// $('.gallery-slider').slick({
+//   autoplay: true,
+//   autoplaySpeed: 3000, // 3 seconds
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+// });
+
+// $('.one-time').slick({
+//   autoplay: true,
+//   autoplaySpeed: 5000, // 5 seconds
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+// });
+console.log('Hello');
+
